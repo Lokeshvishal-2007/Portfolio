1cd7d361109c085ca222e0df0cc6fdf5ab130dd2
Lokeshvishal <rlokeshvishal@gmail.com>
2026-08-24 13:48:57 +0530

    Portfolio

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -237,7 +237,7 @@ function HeroVisual() {
 
           <div className="hero-photo-frame">
             <Image
-              src="public/profile/image.jpg"
+              src="public/profile/Image.jpg"
               alt="Lokesh Vishal"
               width={220}
               height={220}
